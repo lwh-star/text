6404ac965e6c182c3f26a27ad61c9b98580496e3
lwh-star <3298389788@qq.com>
2024-10-16 07:46:28 +0800

    小练习

--- main.c
+++ main.c
@@ -1,51 +1,39 @@
-#include<stdio.h>
-int main() {
-	int maxz = 0, minz = 0, maxf = 0, minf = 0, zs = 0, fs = 0, a = 0;
+#include <stdio.h>
+#include <stdlib.h>
 
-	for (int i = 0; i < 20; i++) {	//循环20次输入
-		scanf("%d", &a);	//输入整数，并记录返回值
-		if (a > 0) {
-
-			if (zs == 0) {
-				maxz = minz = a;
-				zs++;
-				continue;
-			}
-			if (maxz <= a) {
-				maxz = a;
-				continue;
-			}
-			if (minz >= a) {
-				minz = a;
-				continue;
-			}
-			else
-				continue;
-		}
-
-		else if (a < 0) {
-			if (fs == 0) {
-				maxf = minf = a;
-				fs++;
-				continue;
-			}
-			if (maxf <= a) {
-				maxf = a;
-				continue;
-			}
-			if (minf >= a) {
-				minf = a;
-				continue;
-			}
-			else
-				continue;
-		}
-
-		else if (a == 0) {
-			continue;
-
-	}
-
+/*int max(int x,int y)
+{
+    if (x > y)
+    return x;
+    else
+    return y;
 }
-	printf("最大正整数为： %d\n最小正整数为： %d\n最大负整数为： %d\n最小负整数为： %d\n", maxz, minz, maxf, minf);
+int main()
+{
+   int a = 0,b = 0;
+
+    scanf("%d%d",&a,&b);
+
+    int r = max(a,b);
+
+    printf("%d\n",r);
+
+
+    printf("Hello world!\n");
+    return 0;
+}*/
+int main()
+{
+  int x = 0;
+  int y = 0;
+  scanf("%d\n",&x);
+  if (x > 1)
+   y = 1;
+     else if(x==0)
+   y = 0;
+  else
+    y = 1;
+   printf("%d\n",y);
+
+   return 0;
 }
