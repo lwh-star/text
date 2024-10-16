f547b3a8b80c757b37352a02c3b7fc8b458f98f2
lwh-star <3298389788@qq.com>
2024-10-16 19:11:41 +0800

    switch练习

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int day;
+
+   scanf("%d",&day);
+
+   switch (day)
+   {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      printf("weekday\n");
+      break;
+    case 6:
+    case 7:
+      printf("weekend\n");
+      break;
+   }
+   return 0;
+}
